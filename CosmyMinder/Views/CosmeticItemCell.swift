ee81d88f3b00d41dcaf6534a13f2431c74585225
Ksuxa97 <iksuxa97@gmail.com>
2025-07-28 15:30:29 +0300

    добавила черновое отображение списка косметики, переход на экран просмотра инфо о продукте/редактирования инфо. Постаралась соблюсти изолированность презентера и вью

---
+++ CosmyMinder/Views/CosmeticItemCell.swift
@@ -0,0 +1,23 @@
+//
+//  CosmeticItemTableViewCell.swift
+//  CosmyMinder
+//
+//  Created by Kseniya Semenova on 23.07.2025.
+//
+
+import UIKit
+
+class CosmeticItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
