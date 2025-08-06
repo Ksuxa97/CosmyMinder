d637bfbf6e32d0b6378399a061ffd6eaffceba6c
Ksuxa97 <iksuxa97@gmail.com>
2025-08-06 11:08:13 +0300

    made subview folder, finalized picture frame

--- CosmyMinder/Views/AddNewProductManuallyViewController.swift
+++ CosmyMinder/Views/AddNewProductManuallyViewController.swift
@@ -12,11 +12,18 @@ final class AddNewProductManuallyViewController: UIViewController {
     private var productStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .vertical
+        stackView.alignment = .center
         stackView.spacing = 20
         return stackView
     }()
 
     private lazy var productImage = ProductImageView(at: self)
+
+    private var productNameTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Название продукта"
+        return textField
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,23 +32,27 @@ final class AddNewProductManuallyViewController: UIViewController {
 
     private func setupUI() {
         view.backgroundColor = .systemBackground
+
         productStackView.addArrangedSubview(productImage)
+        productStackView.addArrangedSubview(productNameTextField)
         view.addSubview(productStackView)
+
 
         productStackView.translatesAutoresizingMaskIntoConstraints = false
 
         NSLayoutConstraint.activate([
             productStackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
-            productStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
-            productStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
+            productStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            productStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
         ])
 
         productImage.translatesAutoresizingMaskIntoConstraints = false
 
         // 3. Констрейнты для изображения
         NSLayoutConstraint.activate([
-            productImage.heightAnchor.constraint(equalToConstant: 100),
-            productImage.widthAnchor.constraint(equalTo: productImage.heightAnchor)
+            productImage.widthAnchor.constraint(equalTo: productStackView.widthAnchor),
+            productImage.heightAnchor.constraint(equalTo: productStackView.widthAnchor, multiplier: 2/3),
+            productImage.centerXAnchor.constraint(equalTo: productStackView.centerXAnchor)
         ])
     }
 
