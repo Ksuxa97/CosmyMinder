01692e0239458bca63dc6b407ba046a8c0ab7657
Ksuxa97 <iksuxa97@gmail.com>
2025-08-20 17:50:42 +0300

    change UIStackView to UIView

--- CosmyMinder/Views/Subviews/FieldStackView.swift
+++ CosmyMinder/Views/Subviews/FieldStackView.swift
@@ -7,7 +7,14 @@
 
 import UIKit
 
-class FieldStackView: UIStackView {
+class FieldStackView: UIView {
+
+    private let fieldStack: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.spacing = 8
+        return stack
+    }()
 
     private let inputLabel: UILabel = {
         let label = UILabel()
@@ -19,14 +26,9 @@ class FieldStackView: UIStackView {
     let textField: ProductTextField
 
     init (title: String, placeholder: String = "", dataType: InputMode) {
-
         self.inputLabel.text = title
         self.textField = ProductTextField(mode: dataType, placeholder: placeholder)
         super.init(frame: .zero)
-
-        self.axis = .vertical
-        self.spacing = 8
-
         setupUI()
     }
     
@@ -43,13 +45,17 @@ class FieldStackView: UIStackView {
     }
 
     private func setupUI() {
-        self.addArrangedSubview(inputLabel)
-        self.addArrangedSubview(textField)
+        self.addSubview(fieldStack)
+        fieldStack.addArrangedSubview(inputLabel)
+        fieldStack.addArrangedSubview(textField)
 
+        fieldStack.translatesAutoresizingMaskIntoConstraints = false
         textField.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             inputLabel.heightAnchor.constraint(equalToConstant: 22),
-            textField.heightAnchor.constraint(equalToConstant: 44)
+            textField.heightAnchor.constraint(equalToConstant: 44),
+            fieldStack.widthAnchor.constraint(equalTo: self.widthAnchor),
+            fieldStack.heightAnchor.constraint(equalTo: self.heightAnchor)
         ])
     }
 }
